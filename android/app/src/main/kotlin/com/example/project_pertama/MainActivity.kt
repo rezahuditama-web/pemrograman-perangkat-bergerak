2dc19a7dfa15f862455616598e5189c3fdc47cfd
rezahuditama-web <rezahuditama@gmail.com>
2026-09-08 11:20:25 +0700

    merapikan

--- android/app/src/main/kotlin/com/example/project_pertama/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.project_pertama
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/latihan_mandiri_01.dart
+++
@@ -1,5 +0,0 @@
-double luas(double panjang,double lebar) {return panjang * lebar;}
-void main () {
- double luasakhir=luas(9, 3);
- print('persegi panjang luasnya = $luasakhir cm');
-}
